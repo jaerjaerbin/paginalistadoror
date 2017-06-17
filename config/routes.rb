f860c0bbad31c1eee59e304e8abab5d661424c8d
jaerma <jaerma74@yahoo.com>
2017-06-17 02:08:49 +0000

    Agregando archivos nuevos

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  # get 'greeter/hello'
+  
+  # SAME AS ABOVE
+  get 'greeter/hello' => "greeter#hello"
+  get 'greeter/goodbye'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
